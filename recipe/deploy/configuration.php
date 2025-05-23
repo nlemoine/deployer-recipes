90ce30f3d95041301243fa0a72c7ead179ae096f
Nicolas Lemoine <nico.lemoine@gmail.com>
2025-05-23 09:13:39 +0200

    fix: GitHub repo url protocol
    Because @actions/checkout uses `https` by default

--- recipe/deploy/configuration.php
+++ recipe/deploy/configuration.php
@@ -11,6 +11,10 @@ if (empty($repository)) {
     set('repository', function () {
         $process = Process::fromShellCommandline('git remote get-url origin');
         $default = $process->mustRun()->getOutput();
+        if (str_starts_with($default, 'https://github.com')) {
+            $default = str_replace('https://', 'ssh://git@', $default);
+        }
+
         return trim($default);
     });
 }
